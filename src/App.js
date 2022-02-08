1aa7f7ff9195af43acb25140f86626a1185bc2d3
Raghvendra Singh <Raghav26897@gmail.com>
2022-02-08 08:53:30 +0530

    gj

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import MyTable from './Components/MyTable';
+import React from "react";
+import List from "./data";
+import { ListContainer, ListItem } from "./styles";
+import { DragHandle } from "./partials/DragHandle";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-function App() {
+const App = () => {
+  const list = List.getList();
   return (
     <div className="App">
-      <MyTable />
+      <DragDropContext
+        onDragEnd={(param) => {
+          const srcI = param.source.index;
+          const desI = param.destination?.index;
+          if (desI) {
+            list.splice(desI, 0, list.splice(srcI, 1)[0]);
+            List.saveList(list);
+          }
+          console.log(srcI);
+          console.log(desI);
+        }}
+      >
+        <ListContainer>
+          <h1>The List</h1>
+          <Droppable droppableId="droppable-1">
+            {(provided, _) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {list.map((item, i) => (
+                  <Draggable
+                    key={item.id}
+                    draggableId={"draggable-" + item.id}
+                    index={i}
+                  >
+                    {(provided, snapshot) => (
+                      <ListItem
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        style={{
+                          ...provided.draggableProps.style,
+                          boxShadow: snapshot.isDragging
+                            ? "0 0 .4rem #666"
+                            : "none",
+                        }}
+                      >
+                        <DragHandle {...provided.dragHandleProps} />
+                        <span>{item.title}</span>
+                      </ListItem>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </ListContainer>
+      </DragDropContext>
     </div>
   );
-}
+};
 
 export default App;
